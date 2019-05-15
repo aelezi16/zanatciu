77c0bdfaf0bec00c5c3d70efbea0aaaaba9f41c4
aelezi16 <aelezi16@epoka.edu.al>
2019-05-15 20:02:01 +0200

    modified publicaiton model-mapper

--- Backend/src/main/java/com/zanatciu/backend/config/converter/impl/PublicationModelMapper.java
+++ Backend/src/main/java/com/zanatciu/backend/config/converter/impl/PublicationModelMapper.java
@@ -3,17 +3,50 @@ package com.zanatciu.backend.config.converter.impl;
 import com.zanatciu.backend.config.converter.ModelMapper;
 import com.zanatciu.backend.domain.publication.dto.PublicationDto;
 import com.zanatciu.backend.domain.publication.model.Publication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class PublicationModelMapper implements ModelMapper<Publication, PublicationDto> {
+
+    @Value(value = "${server:uri}")
+    private String server;
+    @Value("${server:port}")
+    private String port;
+
     @Override
     public PublicationDto modelToDto(Publication publication) {
-        return new PublicationDto();
+        PublicationDto dto = new PublicationDto();
+
+        dto.setId(publication.getId());
+        dto.setTitle(publication.getTitle());
+        dto.setDescription(publication.getDescription());
+        dto.setRate(publication.getRate());
+        dto.setUsername(publication.getUsername());
+        dto.setRatingCount(publication.getRatingCount());
+        dto.setTimestamp(publication.getTimestamp());
+        dto.setType(publication.getType());
+
+        return dto;
     }
 
     @Override
     public Publication dtoToModel(PublicationDto publicationDto) {
-        return new Publication();
+        Publication model = new Publication();
+
+        if(publicationDto.getId()!=null)
+            model.setId(publicationDto.getId());
+
+        model.setTitle(publicationDto.getTitle());
+        model.setDescription(publicationDto.getDescription());
+        model.setRate(publicationDto.getRate());
+        model.setUsername(publicationDto.getUsername());
+        model.setRatingCount(publicationDto.getRatingCount());
+        model.setTimestamp(new Date());
+        model.setType(publicationDto.getType());
+
+        return model;
     }
 }
