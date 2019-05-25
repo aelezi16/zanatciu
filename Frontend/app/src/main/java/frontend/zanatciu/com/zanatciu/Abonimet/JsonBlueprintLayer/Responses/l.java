2bd14a5c3b52a93c505690b13b819318cfd72737
gerildpjetri <gpjetri16@epoka.edu.al>
2019-05-25 03:12:44 +0200

    Added segmented control buttons in kreu layout and build 4 initial interfaces

--- Frontend/app/src/main/java/frontend/zanatciu/com/zanatciu/Abonimet/JsonBlueprintLayer/Responses/l.java
+++
@@ -1,4 +0,0 @@
-package frontend.zanatciu.com.zanatciu.Abonimet.JsonBlueprintLayer.Responses;
-
-public class l {
-}
--- Frontend/app/src/main/java/frontend/zanatciu/com/zanatciu/Funksionalitete/ViewModels/Login.java
+++ Frontend/app/src/main/java/frontend/zanatciu/com/zanatciu/Funksionalitete/ViewModels/Login.java
@@ -1,5 +1,8 @@
 package frontend.zanatciu.com.zanatciu.Funksionalitete.ViewModels;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +10,18 @@ import frontend.zanatciu.com.zanatciu.R;
 
 public class Login extends AppCompatActivity {
 
+    private ActionBar actionBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login_interface);
+
+        actionBar=getSupportActionBar();
+        actionBar.hide();
+
+
+
     }
 }
