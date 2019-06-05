10c21cb18b79323e0eb20cf551a4c23d15bcf71f
aelezi16 <aelezi16@epoka.edu.al>
2019-06-05 08:13:03 +0200

    added email-sender configuration

--- Backend/src/main/java/com/zanatciu/backend/config/mail/config/SimpMailConfig.java
+++ Backend/src/main/java/com/zanatciu/backend/config/mail/config/SimpMailConfig.java
@@ -1,4 +1,30 @@
 package com.zanatciu.backend.config.mail.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
 public class SimpMailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("z.zanatciu@gmail.com");
+        mailSender.setPassword("arbielezi123456");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 }
--- Backend/src/main/java/com/zanatciu/backend/config/mail/service/SimpMailService.java
+++ Backend/src/main/java/com/zanatciu/backend/config/mail/service/SimpMailService.java
@@ -1,4 +1,33 @@
 package com.zanatciu.backend.config.mail.service;
 
-public class SimpMailService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpMailService{
+
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public SimpMailService(
+            JavaMailSender mailSender
+    ){
+        this.mailSender = mailSender;
+    }
+
+    public void sendSimpleMessage(
+            String to, String subject, String text
+    ){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
+
+
 }
